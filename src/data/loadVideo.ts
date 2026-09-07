6bb72f0f295c60ca14d91f7722ee22a1091d112e
Astroblazter <orlovdove@gmail.com>
2026-09-07 22:39:28 +0300

    Add loadVideo.ts for loading video metadata

---
+++ src/data/loadVideo.ts
@@ -0,0 +1,17 @@
+export type VideoItem = {
+  id: string;
+  poster: string;
+  title: string;
+};
+
+const files = import.meta.glob('/src/content/video/*.json', {
+  eager: true,
+  import: 'default',
+}) as Record<string, Omit<VideoItem, 'id'>>;
+
+export const VIDEOS: VideoItem[] = Object.entries(files)
+  .sort(([a], [b]) => a.localeCompare(b))
+  .map(([path, data]) => {
+    const id = path.split('/').pop()!.replace(/\.json$/, '');
+    return { id, ...data };
+  });
